31c222913ec31d9c07592f3c752650438d9bd181
Samuel Solomon <mrsamuelsolomon@gmail.com>
2021-05-20 06:48:15 +0100

    globalstyle

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
+import styled from 'styled-components'
 
 function App() {
   return (
-    <div className="App">
+    <AppStyled>
       <h1>Styled Components</h1>
-    </div>
+    </AppStyled>
   );
 }
+
+
+const AppStyled = styled.div `
+  background-color: red;
+`;
 
 export default App;
