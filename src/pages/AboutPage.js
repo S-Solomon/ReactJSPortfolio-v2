43fdc3c127420c688b18820b7fe64a3944ba9d9a
Samuel Solomon <mrsamuelsolomon@gmail.com>
2021-05-21 07:59:51 +0100

    title component

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -1,11 +1,21 @@
 import React from 'react'
+import styled from 'styled-components'
+import Title from '../Components/Title'
+import { MainLayout } from '../styles/Layouts'
 
 function AboutPage() {
     return (
-        <div>
-            <h1>AboutPage</h1>
-        </div>
+        <MainLayout>
+            <AboutStyled>
+                <Title title={'About Me'} span={'About Me'}/>
+            </AboutStyled>
+        </MainLayout>
     )
 }
+
+
+const AboutStyled = styled.section `
+
+`;
 
 export default AboutPage
