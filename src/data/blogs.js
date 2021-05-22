5055c5b5fba484cbce098c145feafa83a11c0a34
Samuel Solomon <mrsamuelsolomon@gmail.com>
2021-05-22 19:52:06 +0100

    blogspage

---
+++ src/data/blogs.js
@@ -0,0 +1,41 @@
+import blog1 from '../img/blogs/blog1.svg'
+import blog2 from '../img/blogs/blog2.svg'
+import blog3 from '../img/blogs/blog3.svg'
+import blog4 from '../img/blogs/blog4.svg'
+
+const blogs = [
+    {
+        id: 1,
+        title: 'How To Work from Home',
+        date: '01',
+        month: 'April',
+        image: blog1,
+        link: 'https://nextjs-blog-s-solomon.vercel.app'
+    },
+    {
+        id: 2,
+        title: 'Two Forms of Pre-rendering',
+        date: '01',
+        month: 'April',
+        image: blog2,
+        link: 'https://nextjs-blog-s-solomon.vercel.app/posts/pre-rendering'
+    },
+    {
+        id: 3,
+        title: 'When to Use Static Generation v.s. Server-side Rendering',
+        date: '01',
+        month: 'April',
+        image: blog3,
+        link: 'https://nextjs-blog-s-solomon.vercel.app/posts/ssg-ssr'
+    },
+    {
+        id: 4,
+        title: 'How to use SEO effeciently',
+        date: '01',
+        month: 'April',
+        image: blog4,
+        link: 'https://www.google.com'
+    }
+]
+
+export default blogs;
