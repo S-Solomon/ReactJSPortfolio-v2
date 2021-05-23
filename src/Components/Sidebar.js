50885dc1c974dabc4192e72b04e842262619b4f6
Samuel Solomon <mrsamuelsolomon@gmail.com>
2021-05-23 17:48:40 +0100

    sidebar edit

--- src/Components/Sidebar.js
+++ src/Components/Sidebar.js
@@ -2,9 +2,9 @@ import React from 'react'
 import styled from 'styled-components'
 import Navigation from '../Components/Navigation'
 
-function Sidebar() {
+function Sidebar({ navToggle }) {
     return (
-        <SidebarStyled>
+        <SidebarStyled className={`${navToggle ? 'nav-toggle' : ''}`}>
             <Navigation />
         </SidebarStyled>
     )
@@ -16,6 +16,12 @@ const SidebarStyled = styled.div `
     width: 16.3rem;
     height: 100vh;
     background-color: var(--sidebar-dark-color);
+    overflow: hidden;
+    transition: all .4s ease-in-out;
+    @media screen and (max-width:1200px){
+        transform: translateX(-100%);
+        z-index: 20;
+    }
 `;
 
 
