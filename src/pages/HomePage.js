98b43d3421b9f88404e5deaaf832798e6a71a9a2
Samuel Solomon <mrsamuelsolomon@gmail.com>
2021-05-24 18:41:14 +0100

    global styles

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -39,13 +39,13 @@ const HomePageStyled = styled.header `
     height: 100vh;
     position: relative;
 
-    .particle-con {
+    /* .particle-con {
         position: absolute;
         top: 0;
         left: 0;
         width: 100%;
         min-height: 100vh;
-    }
+    } */
 
     .typography {
         position: absolute;
